82aed85581d619a2b89b9c95bc44b79ddde4fffd
Austin Roberts <austin.roberts@rivet.cloud>
2024-02-27 09:53:32 -0600

    Initial commit

---
+++ module.go
@@ -0,0 +1,186 @@
+package din
+
+import (
+	"fmt"
+	"strings"
+	"net/http"
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp/reverseproxy"
+	"net/url"
+)
+
+var (
+	_ caddy.Module                = (*DinUpstreams)(nil)
+	_ caddy.Provisioner           = (*DinUpstreams)(nil)
+	// _ caddy.Validator             = (*DinUpstreams)(nil)
+	_ caddyfile.Unmarshaler       = (*DinUpstreams)(nil)
+	_ reverseproxy.UpstreamSource = (*DinUpstreams)(nil)
+)
+
+
+func init() {
+	caddy.RegisterModule(DinUpstreams{})
+	caddy.RegisterModule(DinSelect{})
+}
+
+type metaUpstream struct{
+	HttpUrl string `json:"http.url"`
+	path    string
+	upstream *reverseproxy.Upstream
+}
+
+func urlToMetaUpstream(urlstr string) (*metaUpstream, error) {
+	url, err := url.Parse(urlstr)
+	if err != nil {
+		return nil, err
+	}
+	return &metaUpstream{
+		HttpUrl: urlstr,
+		path: url.Path,
+		// upstream: &reverseproxy.Upstream{Dial: fmt.Sprintf("%v://%v", url.Scheme, url.Host)},
+		upstream: &reverseproxy.Upstream{Dial: url.Host},
+	}, nil
+}
+
+type DinSelect struct {
+	selector reverseproxy.Selector
+}
+
+func (d *DinSelect) Provision(context caddy.Context) error {
+	selector := &reverseproxy.HeaderHashSelection{Field: "Din-Session-Id"}
+	selector.Provision(context)
+	d.selector = selector
+	return nil
+}
+
+// CaddyModule returns the Caddy module information.
+func (DinSelect) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.reverse_proxy.selection_policies.dinupstreams",
+		New: func() caddy.Module { return new(DinSelect) },
+	}
+}
+
+func (d *DinSelect) Select(pool reverseproxy.UpstreamPool, r *http.Request, rw http.ResponseWriter) *reverseproxy.Upstream {
+	// }
+
+	repl := r.Context().Value(caddy.ReplacerCtxKey).(*caddy.Replacer)
+	var mus []*metaUpstream
+	if v, ok := repl.Get("din.internal.upstreams"); ok {
+		mus = v.([]*metaUpstream)
+	}
+
+	res := d.selector.Select(pool, r, rw)
+	for _, mu := range mus {
+		if res == mu.upstream {
+			r.URL.RawPath = mu.path
+			r.URL.Path, _ = url.PathUnescape(r.URL.RawPath)
+			break
+		}
+	}
+	return res
+}
+
+func (d *DinSelect) UnmarshalCaddyfile(dispenser *caddyfile.Dispenser) error {
+	return nil
+}
+
+
+
+// Gizmo is an example; put your own type here.
+type DinUpstreams struct {
+	Services map[string][]*metaUpstream `json:"Services"`
+	Methods map[string][]*string `json:"Methods"`
+}
+
+func (d *DinUpstreams) Provision(context caddy.Context) error {
+	for _, upstreams := range d.Services {
+		for _, metaUpstream := range upstreams {
+			url, err := url.Parse(metaUpstream.HttpUrl)
+			if err != nil {
+				return err
+			}
+			// metaUpstream.upstream = &reverseproxy.Upstream{Dial: fmt.Sprintf("%v://%v", url.Scheme, url.Host)}
+			metaUpstream.upstream = &reverseproxy.Upstream{Dial: url.Host}
+			metaUpstream.path = url.Path
+		}
+	}
+	return nil
+}
+
+func (d *DinUpstreams) GetUpstreams(r *http.Request) ([]*reverseproxy.Upstream, error) {
+	mus, ok := d.Services[strings.TrimPrefix(r.URL.Path, "/")]
+	if !ok {
+		fmt.Printf("No services available for %v\n", strings.TrimPrefix(r.URL.Path, "/"))
+		return nil, fmt.Errorf("service undefined")
+	}
+	res := make([]*reverseproxy.Upstream, len(mus))
+	for i, u := range mus {
+		res[i] = u.upstream
+	}
+
+	repl := r.Context().Value(caddy.ReplacerCtxKey).(*caddy.Replacer)
+	repl.Set("din.internal.upstreams", mus)
+
+	return res, nil
+}
+
+// CaddyModule returns the Caddy module information.
+func (DinUpstreams) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.reverse_proxy.upstreams.dinupstreams",
+		New: func() caddy.Module { return new(DinUpstreams) },
+	}
+}
+
+func (d *DinUpstreams) UnmarshalCaddyfile(dispenser *caddyfile.Dispenser) error {
+	if d.Methods == nil {
+		d.Methods = make(map[string][]*string)
+	}
+	if d.Services == nil {
+		d.Services = make(map[string][]*metaUpstream)
+	}
+	for dispenser.Next() { // Skip the directive name
+		switch dispenser.Val() {
+		case "services":
+			for n1 := dispenser.Nesting(); dispenser.NextBlock(n1); {
+				serviceName := dispenser.Val()
+				fmt.Println(serviceName)
+	
+				for nesting := dispenser.Nesting(); dispenser.NextBlock(nesting); {
+					switch dispenser.Val() {
+					case "methods":
+						d.Methods[serviceName] = make([]*string, dispenser.CountRemainingArgs())
+						for i := 0; i < dispenser.CountRemainingArgs(); i++ {
+							d.Methods[serviceName][i] = new(string)
+						}
+						if !dispenser.Args(d.Methods[serviceName]...) {
+							return dispenser.Errf("invalid 'methods' argument for service %s", serviceName)
+						}
+					case "providers":
+						if dispenser.NextBlock(nesting + 1) {
+							ms, err := urlToMetaUpstream(dispenser.Val())
+							if err != nil {
+								return err
+							}
+							d.Services[serviceName] = append(d.Services[serviceName], ms)
+							for dispenser.NextBlock(nesting + 1) {
+								ms, err := urlToMetaUpstream(dispenser.Val())
+								if err != nil {
+									return err
+								}
+								d.Services[serviceName] = append(d.Services[serviceName], ms)
+							}
+						} else {
+							return dispenser.Errf("expected at least one provider for service %s", serviceName)
+						}
+					default:
+						return dispenser.Errf("unrecognized option: %s", dispenser.Val())
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
