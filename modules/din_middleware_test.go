6c5afceee813f2f2340cb98ca883209e751604c6
Nate Fikru <natefikru@gmail.com>
2024-05-03 12:49:42 -0400

    added serve http test

--- modules/din_middleware_test.go
+++ modules/din_middleware_test.go
@@ -1,10 +1,15 @@
 package modules
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
 	reflect "reflect"
 	"testing"
 
 	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp/reverseproxy"
 )
 
@@ -31,6 +36,57 @@ func TestMiddlewareCaddyModule(t *testing.T) {
 			}
 			if reflect.TypeOf(modInfo.New()) != reflect.TypeOf(tt.output.New()) {
 				t.Errorf("CaddyModule() = %v, want %v", modInfo.New(), tt.output.New())
+			}
+		})
+	}
+}
+
+func TestMiddlewareServeHTTP(t *testing.T) {
+	dinMiddleware := new(DinMiddleware)
+
+	test := []struct {
+		name     string
+		request  *http.Request
+		services map[string][]*metaUpstream
+		hasErr   bool
+	}{
+		{
+			name:    "successful request",
+			request: httptest.NewRequest("GET", "http://localhost:8000/eth", nil),
+			services: map[string][]*metaUpstream{
+				"eth": {
+					&metaUpstream{},
+				},
+			},
+			hasErr: false,
+		},
+		{
+			name:    "unsuccessful request, path not found",
+			request: httptest.NewRequest("GET", "http://localhost:8000/xxx", nil),
+			services: map[string][]*metaUpstream{
+				"eth": {
+					&metaUpstream{},
+				},
+			},
+			hasErr: true,
+		},
+		{
+			name:     "unsuccessful request, service map is empty",
+			request:  httptest.NewRequest("GET", "http://localhost:8000/eth", nil),
+			services: map[string][]*metaUpstream{},
+			hasErr:   true,
+		},
+	}
+
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			dinMiddleware.Services = tt.services
+			tt.request = tt.request.WithContext(context.WithValue(tt.request.Context(), caddy.ReplacerCtxKey, caddy.NewReplacer()))
+			rw := httptest.NewRecorder()
+
+			err := dinMiddleware.ServeHTTP(rw, tt.request, caddyhttp.HandlerFunc(func(w http.ResponseWriter, r *http.Request) error { return nil }))
+			if err != nil && !tt.hasErr {
+				t.Errorf("ServeHTTP() = %v, want %v", err, tt.hasErr)
 			}
 		})
 	}
