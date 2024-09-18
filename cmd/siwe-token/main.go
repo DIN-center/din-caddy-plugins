8eda8b0b0a58e0a727da6c4e57440354a2b86e2e
Nate Fikru <natefikru@gmail.com>
2024-09-18 19:52:46 -0400

    added comments and verbose logs

--- cmd/siwe-token/main.go
+++ cmd/siwe-token/main.go
@@ -1,44 +1,56 @@
 package main
 
 import (
-	"fmt"
-	"github.com/openrelayxyz/din-caddy-plugins/lib/auth/siwe"
-	"os"
-	"io/ioutil"
-	"strings"
 	"encoding/hex"
-)
+	"fmt"
+	"os"
+	"strings"
 
+	"github.com/openrelayxyz/din-caddy-plugins/lib/auth/siwe"
+	"go.uber.org/zap"
+)
 
 func main() {
 	url := os.Args[1]
 	keyFile := os.Args[2]
-	
 
-	hexKeyBytes, err := ioutil.ReadFile(keyFile)
+	// Read the key file
+	hexKeyBytes, err := os.ReadFile(keyFile)
 	if err != nil {
-		panic(fmt.Sprintf("Failed to read secrets file: %v", err.Error()))
+		panic(fmt.Sprintf("Failed to read secret key file: %v", err.Error()))
 	}
+	// Trim the 0x prefix and any whitespace
 	hexKey := string(hexKeyBytes)
 	hexKey = strings.TrimSpace(strings.TrimPrefix(hexKey, "0x"))
 	key, err := hex.DecodeString(hexKey)
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("Failed to decode secret key: %v", err.Error()))
 	}
+
+	// Create a new signer with the key
 	signer := &siwe.SigningConfig{
 		PrivateKey: key,
-		SignerURL: "http://localhost",
+		SignerURL:  "http://localhost",
 	}
+
+	// Generate a new keypair
 	signer.GenPrivKey()
+
+	// Print the address
 	os.Stderr.WriteString("Your signing address: ")
 	os.Stderr.WriteString(signer.Address)
 	os.Stderr.WriteString("\n")
 
+	// Create a new SIWE client
 	client := siwe.NewSIWEClient(url, 0, signer)
-	client.Start(nil)
+
+	// Start the client
+	client.Start(zap.NewNop())
+
+	// Get a token from the client
 	token, err := client.GetToken(nil)
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("Failed to get token: %v", err.Error()))
 	}
 	result := make([]string, 0, len(token.Headers))
 	for k, v := range token.Headers {
