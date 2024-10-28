da0193cd360be5243a0f4be6446b118e296bb9f1
Nate Fikru <natefikru@gmail.com>
2024-10-28 18:51:42 -0400

    created generic request struct, properly unmarshal json

--- modules/types.go
+++ modules/types.go
@@ -43,3 +43,10 @@ func (h HealthStatus) String() string {
 		return "Unknown"
 	}
 }
+
+type JSONRPCRequest struct {
+	Method  string        `json:"method"`
+	Params  []interface{} `json:"params"`
+	ID      int           `json:"id"`
+	JSONRPC string        `json:"jsonrpc"`
+}
