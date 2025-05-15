52f45fd4291d5656bbf6f78c8faf03e6d2d81125
Nate Fikru <natefikru@gmail.com>
2025-05-15 13:50:15 -0400

    removed extra fields in the solana and evm block data response

--- lib/http/types.go
+++ lib/http/types.go
@@ -12,29 +12,7 @@ type JSONRPCRequest struct {
 }
 
 type EVMBlockResult struct {
-	BaseFeePerGas         string `json:"baseFeePerGas"`
-	BlobGasUsed           string `json:"blobGasUsed"`
-	Difficulty            string `json:"difficulty"`
-	ExcessBlobGas         string `json:"excessBlobGas"`
-	ExtraData             string `json:"extraData"`
-	GasLimit              string `json:"gasLimit"`
-	GasUsed               string `json:"gasUsed"`
-	Hash                  string `json:"hash"`
-	LogsBloom             string `json:"logsBloom"`
-	Miner                 string `json:"miner"`
-	MixHash               string `json:"mixHash"`
-	Nonce                 string `json:"nonce"`
-	Number                string `json:"number"`
-	ParentBeaconBlockRoot string `json:"parentBeaconBlockRoot"`
-	ParentHash            string `json:"parentHash"`
-	ReceiptsRoot          string `json:"receiptsRoot"`
-	Sha3Uncles            string `json:"sha3Uncles"`
-	Size                  string `json:"size"`
-	StateRoot             string `json:"stateRoot"`
-	Timestamp             string `json:"timestamp"`
-	TotalDifficulty       string `json:"totalDifficulty"`
-	TransactionsRoot      string `json:"transactionsRoot"`
-	WithdrawalsRoot       string `json:"withdrawalsRoot"`
+	Hash string `json:"hash"`
 }
 
 type JSONRPCEVMBlockResponse struct {
@@ -50,9 +28,5 @@ type JSONRPCSolanaBlockResponse struct {
 }
 
 type SolanaBlockResult struct {
-	BlockHeight       int    `json:"blockHeight"`
-	BlockTime         int    `json:"blockTime"`
-	Blockhash         string `json:"blockhash"`
-	ParentSlot        int    `json:"parentSlot"`
-	PreviousBlockhash string `json:"previousBlockhash"`
+	Blockhash string `json:"blockhash"`
 }
