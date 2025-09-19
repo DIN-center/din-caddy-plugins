449525db3eed69c8278049001947eef724f9c655
Nate Fikru <natefikru@gmail.com>
2025-09-19 12:01:16 -0400

    refactor the min function

--- lib/network/beacon_handler.go
+++ lib/network/beacon_handler.go
@@ -751,10 +751,3 @@ func getBoolFromMetadata(metadata map[string]interface{}, key string) bool {
 	return false
 }
 
-// Helper function to get minimum of two integers
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
